d8861b80d433c4aa6ac9ec3737081c37ce386d60
Olivier_Luethy <olivier@kauz.ch>
2023-02-09 08:42:20 +0100

    Added comments to logout

--- logout.php
+++ logout.php
@@ -1,8 +1,13 @@
 <?php
+  // Start the session
   session_start();
+  // Include the 'index.php' file
   include 'index.php';
 
+  // Unset the 'access_token' session
   unset($_SESSION['access_token']);
+  // Revoke the access token from Google API
   $client->revokeToken();
+  // Redirect the user back to the index page
   header('Location: index.php');
 ?>
